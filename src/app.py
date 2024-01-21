07a940cda477fe59476e816967157e18472e61c9
Hakushine <hakushine.ryu.here@gmail.com>
2024-01-22 03:33:49 +0900

    base program clear

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-import json
 from io import BytesIO
 
 import numpy as np
@@ -10,22 +9,10 @@ from src.widgets import create_textbox
 
 st.title("Create Bar Graph!")
 
-# TODO: 前回の設定値をjsonを読み出すようにする
-# 前回の設定値を読み出す
-st.markdown(
-    "# If you have saved your previous use in json format,"
-    " you can retrieve it."
-)
-config_json_path = st.file_uploader("graph_config.json", type="json")
-if config_json_path is not None:
-    with open(config_json_path, mode="rt", encoding="utf-8") as f:
-        graph_cfgs: dict[str, str | float] = json.load(f)
-else:
-    graph_cfgs = {}
 st.markdown("# Gpaph configuration")
 with st.form(key="config_form"):
-    # ファイルの選択
-    st.markdown("### Upload your csv file")
+    # ファイルのアップロード
+    st.markdown("## Upload your csv file")
     csv_path = st.file_uploader("csv file", type="csv")
     # ヘッダー（カラム名）を含むか
     has_header = st.checkbox(
@@ -44,40 +31,48 @@ with st.form(key="config_form"):
         )
         filename = f"{csv_path.name.split('.')[0]}"
 
-    st.markdown("### Unit conversion")
+    st.markdown("## Unit conversion")
     nm_per_px = st.text_input("unit: nm / pixel", value="1", key="nm_per_px")
 
     # configuration of x range
-    st.markdown("### Set x-axis range")
+    st.markdown("## Set x-axis Configuration")
     cols_x_minmax = st.columns(2)
     with cols_x_minmax[0]:
         x_min = create_textbox(
             "x-min",
             float,
-            default_value="nan" if config_json_path is None else graph_cfgs["x_min"],
+            default_value="nan",
             key="x_min"
         )
-        if config_json_path is not None:
-            graph_cfgs["x_min"] = x_min
 
     with cols_x_minmax[1]:
         x_max = create_textbox(
             "x-max",
             float,
-            default_value="nan" if config_json_path is None else graph_cfgs["x_max"],
-            key="x_max"
+            default_value="nan",
+            key="x_max",
         )
-        if config_json_path is not None:
-            graph_cfgs["x_max"] = x_max
     histogram_step = create_textbox(
         "step",
         value_type=float,
         default_value="1",
-        key="step"
+        key="step",
+    )
+    bin_width = create_textbox(
+        "width of bins",
+        value_type=float,
+        default_value="0.8",
+        key="bin_width"
+    )
+    x_label = create_textbox(
+        "label name for x-axis",
+        value_type=str,
+        default_value="x",
+        key="x_label",
     )
 
     # configuration of y range
-    st.markdown("### Set y-axis range")
+    st.markdown("## Set y-axis Configuration")
     cols_y_minmax = st.columns(2)
     with cols_y_minmax[0]:
         y_min = create_textbox(
@@ -93,6 +88,44 @@ with st.form(key="config_form"):
             default_value="nan",
             key="y_max"
         )
+    y_label = create_textbox(
+        "label name for y-axis",
+        value_type=str,
+        default_value="y",
+        key="y_label",
+    )
+
+    st.markdown("## Optional configs")
+    cols_legend = st.columns(2)
+    with cols_legend[0]:
+        is_showing_legend = st.checkbox(
+            "Showing the legends",
+            value=False,
+            key="is_showing_legend",
+        )
+    with cols_legend[1]:
+        legend = create_textbox(
+            "legend",
+            str,
+            default_value="" if filename is None else filename,
+            key="legend",
+        )
+
+    cols_xtickslabels = st.columns(2)
+    with cols_xtickslabels[0]:
+        is_set_xticklabels = st.checkbox(
+            "changing the x-axis scale to classes",
+            value=False,
+            key="is_set_xticklabels",
+        )
+
+    with cols_xtickslabels[1]:
+        label_angle = create_textbox(
+            "Angle of label",
+            float,
+            default_value="0",
+            key="label_angle",
+        )
 
     is_created_fig = st.form_submit_button("create graph")
 
@@ -100,16 +133,18 @@ st.markdown("# Showing graph")
 if data is None:
     st.caption("Please select valid csv file.")
 else:
-    # TODO: 上で設定したやつを適応する
-    # Display figure
-    # fig = plt.figure()
-    # ax = fig.add_subplot()
-    # ax.plot(data[:, 0], data[:, 1])
     fig = plot_hist(
-        data,
+        x=data,
         xlim=(x_min, x_max),
         ylim=(y_min, y_max),
         step=histogram_step,
+        bin_width=bin_width,
+        x_label=x_label,
+        y_label=y_label,
+        label_angle=label_angle,
+        legend=legend,
+        is_set_xticklabels=is_set_xticklabels,
+        is_showing_legend=is_showing_legend,
     )
 
     st.pyplot(fig)
@@ -147,8 +182,3 @@ else:
         buf.close()
     else:
         st.text("⚠️: Invalid value")
-
-# TODO: 設定値をjson形式で保存できるようにする
-        st.text("⚠️: Invalid value")
-
-# TODO: 設定値をjson形式で保存できるようにする
